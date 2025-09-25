c4fa8278389484e8788b28b5c9a3529926297f36
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2025-09-25 18:56:30 +0100

    feat: add store signature
    This adds a key to nixbox for it to sign its derivation.
    This machine’s public key can be trusted by other machines to allow
    nixbox to deploy updates to them.

--- hosts/linux/nixbox/default.nix
+++ hosts/linux/nixbox/default.nix
@@ -1,7 +1,15 @@
-{ lib, pathRoot, ... }: {
+{ lib, pathRoot, config, ... }: {
   imports = [
     ./disk.nix
   ];
+
+  sops.secrets.store-key = lib.mkDefault {
+    sopsFile = pathRoot + "/secrets/nixbox.store-key.sops";
+    format = "binary";
+    mode = "0400";
+  };
+  nix.settings.secret-key-files = config.sops.secrets.store-key.path;
+  nix.settings.trusted-public-keys = [ "nixbox-1:+RhEM+GSeQmbFCaadAv6fQiuWzAF6f1FW4yuFhfHmYI=" ];
 
   virtualisation.virtualbox.guest.enable = true;
 
